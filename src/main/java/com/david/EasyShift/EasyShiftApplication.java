a4423d45a62cdd257cef487f89e4dabf5cb27ba9
David Desse <daviddesse2@gmail.com>
2024-01-20 18:57:31 +0200

    with swagger

---
+++ src/main/java/com/david/EasyShift/EasyShiftApplication.java
@@ -0,0 +1,13 @@
+package com.david.EasyShift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyShiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/EasyShift/EasyShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.EasyShift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
